125556247f9d71f1af06b913ff25dfede15a1f7e
Ignacio Semerene <igna14@gmail.com>
2025-07-03 18:38:31 -0600

    chore: clean config for Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  base: '/anotan_ventures/',
-  site: 'https://igna14.github.io'
+  base: '/',
+  site: 'https://anotan.com',
 });
